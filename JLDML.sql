382ebd7bae101931971169b7262c76534bb4c065
LinnEdvardsson <linn.edvardsson@yh.nackademin.se>
2025-01-22 13:53:51 +0100

    Update JLDML.sql

--- JLDML.sql
+++ JLDML.sql
@@ -1,4 +1,4 @@
--- vilka kunder som handlar svarta dr martens
+
 select Specification.color, Specification.brand, Customer.firstName
 from CustomerOrder
 inner join Customer on CustomerOrder.customerId = Customer.id
@@ -7,14 +7,12 @@ inner join Product on OrderedProduct.productId = Product.id
 inner join Specification on Product.specId = Specification.id
 where Specification.color = 'White' and Specification.brand = 'Adidas';
 
--- produkter i varje kategori
--- right join för att visa kategorier som än inte har några produkter.
+
 select Category.categoryName, count(ProductInCategory.categoryId) as productCount from ProductInCategory 
 right join Category on Category.id = ProductInCategory.categoryId
 group by Category.categoryName, ProductInCategory.categoryId;
 
--- totalen varje kund handlat för
--- left join för att få med de kunder som än inte lagt någon order. 
+
 select Customer.firstName, Customer.lastName, sum(OrderedProduct.quantity) as productsOrdered, count(distinct CustomerOrder.id) as ordersPlaced, sum(OrderedProduct.quantity * Specification.price) as total from Customer
 left join CustomerOrder on CustomerOrder.customerId = Customer.id 
 left join OrderedProduct on OrderedProduct.orderId = CustomerOrder.id
@@ -22,7 +20,6 @@ left join Product on Product.id = OrderedProduct.productId
 left join Specification on Specification.id = Product.specId
 group by Customer.id;
 
--- total försäljning i varje stad
 select Location.city, sum(OrderedProduct.quantity * Specification.price) as total from Customer
 inner join Location on Location.id = Customer.locationId
 inner join CustomerOrder on CustomerOrder.customerId = Customer.id
@@ -32,7 +29,7 @@ inner join Specification on Specification.id = Product.specId
 group by Location.id
 having total > 1000;
 
--- top 5 sålda produkter
+
 select Product.productName, Specification.brand, sum(OrderedProduct.quantity) as sold from OrderedProduct
 inner join Product on Product.id = OrderedProduct.productId
 inner join Specification on Specification.id = Product.specId
@@ -40,7 +37,6 @@ group by Product.id
 order by sold desc 
 limit 5;
 
--- månad med störst försäljning
 select sum(OrderedProduct.quantity * Specification.price) as totalSales, count(distinct CustomerOrder.id) as totalOrders, date_format(CustomerOrder.dateOfOrder, '%Y-%M') as month
 from CustomerOrder
 inner join OrderedProduct on OrderedProduct.orderId = CustomerOrder.id
